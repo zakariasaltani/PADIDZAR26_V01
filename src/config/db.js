e22d0989d19d1eae0a7c20001248b011cbd5f422
zakariasaltani <41652730+zakariasaltani@users.noreply.github.com>
2026-03-12 00:55:57 +0000

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
-
 const { Pool } = require("pg");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL manquante");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
-module.exports = pool;
-
-module.exports = pool;
+module.exports = pool;;
