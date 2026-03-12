b622a340074f252085a887f231791c08168f8d74
zakariasaltani <41652730+zakariasaltani@users.noreply.github.com>
2026-03-12 00:27:45 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3000;
-
+app.use(express.static("public"));
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Serveur lancé sur http://0.0.0.0:${PORT}`);
 });
 
-start();
+
